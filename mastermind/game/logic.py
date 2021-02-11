3d24052478b4026c4e6a1582b49945c8e53fb0a6
Tyler Shellman <she17007@byui.edu>
2021-02-11 15:48:46 -0700

    Updated Logic to represent correct passing of object: roster.current_player

--- mastermind/game/logic.py
+++ mastermind/game/logic.py
@@ -7,13 +7,12 @@ class Logic():
     match or are in the right spot.
     """
     def __init__(self):
-        self.guess = [1, 2, 3, 4] ### TODO: This will need to be changed to match the other classes ###
         self.masterNum = []
         self.result = []
-        self.prepare_number()
+        self._prepare_number()
         
 
-    def prepare_number(self):
+    def _prepare_number(self):
         """This method will run on instantion and will 
         generate a random four digit number.
 
@@ -24,21 +23,22 @@ class Logic():
         for _ in range(4):
             x = random.randint(0, 9)
             self.masterNum.append(x)
-        # #print(self.masterNum)
+        # print(self.masterNum)
 
-    def check_numbers(self):
+    def check_number(self, guess):
         """This method will compare the number to the users guess and
         create a hint using "x's", "o's", and "*".
 
         Args:
             Self: an instance of Logic
         """
+        #guess = roster.current_player() ##TODO: This line will (in the future) pick the object representing the players guess
         for i in range(4):
             #first loop will iterate through each location of the masterNum
             inList = False # used for checking if each number guessed is in the masterNum list
             for x in range(4):
                 # second loop compares the digit i in masterNum with each digit in the guess
-                if self.masterNum[i] == self.guess[x]:
+                if self.masterNum[i] == guess[x]:
                     inList = True
                     if i == x:
                         self.result.append("x") # right number & location
@@ -47,8 +47,9 @@ class Logic():
             if inList == False:
                     self.result.append("*") # not in the list
                     
-        # #print(self.result)
+        # print(self.result)
 
 
-# #run = Logic()
-# #run.check_numbers()
+# guess = [1, 2, 3, 4]
+# run = Logic()
+# run.check_number(guess)
