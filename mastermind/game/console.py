d65acf7bde215cb75e077fb7a236ec09987b8efc
robbieswan <64027345+robbieswan@users.noreply.github.com>
2021-02-13 17:53:46 -0700

    Created console.

---
+++ mastermind/game/console.py
@@ -0,0 +1,55 @@
+import random
+
+class Console:
+    """A code template for a computer console. The responsibility of this 
+    class of objects is to get text or numerical input and display text output.
+
+
+    Attributes:
+        prompt (string): The prompt to display on each line.
+    """
+    
+     
+    def read(self, prompt):
+        """Prints out the name of each Players
+
+        Args: 
+            self (Screen): An instance of Screen.
+            prompt (string): The prompt to display to the user.
+
+        Returns:
+            string: The user's input as text.
+        """
+        
+        return input(prompt)
+      
+      
+    def blank_turn(self, prompt):
+      """ Prints out blank terminal for each player at the
+      beginning of game.
+      
+      Get's blank terminal from player
+      """
+      
+      print(prompt)
+
+    def read_number(self, prompt):
+        """Gets numerical input from the user through the screen.
+
+        Args: 
+            self (Screen): An instance of Screen.
+            prompt (string): The prompt to display to the user.
+
+        Returns:
+            integer: The user's input as an integer.
+        """
+        return int(input(prompt))
+        
+    def write(self, text):
+        """Displays the winner text on the screen from director.
+
+        Args: 
+            self (Screen): An instance of Screen.
+            text (string): The text to display.
+        """
+        print(text)
